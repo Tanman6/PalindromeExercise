6cb7b967d254fe6817a951e1fdd4ec5344d3c311
Tanman6 <tanmansyx@protonmail.com>
2022-03-07 13:51:19 -0600

    init commit

---
+++ PalindromeExercise/PalindromeExercise/PalindromeExercise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PalindromeExercise
+{
+
+        public class WordSmith
+        {
+            public bool IsAPalindrome(string str)
+            {
+            var reverse = "";
+
+                for (int i = str.Length -1; i >= 0; i--)
+                {
+                reverse += str[i];
+                }
+
+            if (reverse.ToLower() == str.ToLower()) return true;
+            return false;
+            }
+        }
+    
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Testset", true)]
+        [InlineData("Rotor", true)]
+        [InlineData("Name", false)]
+        public void IsAPalindrome(string str, bool expected)
         {
-
+            //Arrange
+            var wordSmith = new WordSmith();
+            //Act
+            bool actual = wordSmith.IsAPalindrome(str);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
